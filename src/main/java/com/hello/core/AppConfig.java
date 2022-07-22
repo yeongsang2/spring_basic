b93e28618ec3bec7d5be7047685b7359838bd8da
yeongsang2 <dudtkd1221@gmail.com>
2022-07-22 11:08:38 +0900

    appconfig 리팩토링

--- src/main/java/com/hello/core/AppConfig.java
+++ src/main/java/com/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.hello.core;
 
+import com.hello.core.dicount.DiscountPolicy;
 import com.hello.core.dicount.FixDiscountPolicy;
+import com.hello.core.member.MemberRepository;
 import com.hello.core.member.MemberService;
 import com.hello.core.member.MemberServiceImpl;
 import com.hello.core.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import com.hello.core.order.OrderServiceImpl;
 public class AppConfig { //applicatin 에 대한 환경 설정
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+    private MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
+
 
 }
