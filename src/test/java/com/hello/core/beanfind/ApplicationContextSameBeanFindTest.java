f819874394266b872082b13e8018b57afa0c9bfa
yeongsang2 <dudtkd1221@gmail.com>
2022-07-26 15:33:07 +0900

    스프링 빈 조회- 동일한 타입이 둘 이상

---
+++ src/test/java/com/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -0,0 +1,58 @@
+package com.hello.core.beanfind;
+
+import com.hello.core.AppConfig;
+import com.hello.core.dicount.DiscountPolicy;
+import com.hello.core.member.MemberRepository;
+import com.hello.core.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextSameBeanFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류 발생") // -> 빈 이름으로 조회
+    void findBeanByTypeDuplicate(){
+        assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> ac.getBean( MemberRepository.class));
+    }
+
+    @Test
+    @DisplayName("특정 타입을 모두 조회") // -> 빈 이름으로 조회
+    void findBeanByType(){
+        Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
+        for (String s : beansOfType.keySet()) {
+            System.out.println("key = " + s + " value = " + beansOfType.get(s));
+        }
+
+        assertThat(beansOfType.size()).isEqualTo(2);
+    }
+
+    @Configuration
+    static class SameBeanConfig {
+
+        @Bean
+        public MemberRepository memberRepository1(){
+            return new MemoryMemberRepository();
+        }
+
+        @Bean
+        public MemberRepository memberRepository2(){
+            return new MemoryMemberRepository();
+        }
+
+    }
+
+
+}
